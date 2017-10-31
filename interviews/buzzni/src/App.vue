d91932caa653c3c90c7c96a0d569a9c3ae6d22da
Jungmin Ji <fdev.jm@gmail.com>
2017-10-31 13:48:23 +0900

    coded until selecting gender and question

--- interviews/buzzni/src/App.vue
+++ interviews/buzzni/src/App.vue
@@ -6,6 +6,7 @@
         template(v-for="el in getGender")
           button(type="button"
           class="btn"
+          @click="selectGender(el)"
           :name="el"
           :value="el") {{ el === 'male' ? '남성' : '여성' }}
       div(class="row")
@@ -15,16 +16,17 @@
           class="btn"
           :name="el"
           :value="el") {{ `${el}대` }}
-      div(class="row")
+      div(class="row last")
         span(aria-label="문항" class="label question") 문항
         template(v-for="n in getQuestionLength")
           button(type="button"
           class="btn"
+          @click="selectQuestion(n)"
           :name="`question${n}`"
           :value="`question${n}`") {{ `${n}번문항` }}
 
-      div(class="chart_container")
-        h2 상단의 옵션을 선택하면
+      div(class="chart_container flex_center")
+        h2(class="chart_heading") 상단의 옵션을 선택하면
           br
           | 그래프가 보여집니다.
 </template>
@@ -43,7 +45,10 @@ export default {
     ...mapGetters(['getGender', 'getAge', 'getQuestionLength']),
   },
   methods: {
-    ...mapActions(['updateSurveyData', 'updateSurveyActionData']),
+    ...mapActions(['updateSurveyData',
+    'updateSurveyActionData',
+    'selectGender',
+    'selectQuestion']),
   },
 }
 </script>
@@ -56,5 +61,55 @@ export default {
 
 html, body
   font-family: 'Nanum Gothic', sans-serif
+  font-size: 10px
 
+#app
+  padding: 25px 30px 30px
+  background-color: #eee
+  max-width: 800px
+  box-sizing: border-box
+  margin: 0 auto
+
+.row
+  margin-bottom: 15px
+  &.last
+    margin-bottom: 25px
+
+.label
+  display: inline-block
+  line-height: 30px
+  font-size: 1.3rem
+  font-weight: bold
+  margin-right: 25px
+  color: #000
+
+.btn
+  width: 72px
+  line-height: 30px
+  color: #fff
+  background-color: rgba(82, 77, 83, 0.5)
+  border: none
+  border-radius: 3px
+  margin-right: 10px
+  font-size: 1.1rem
+  &:last-child
+    margin: 0
+
+.chart_container
+  position: relative
+  background-color: #fff
+  max-width: 740px
+  height: 440px
+
+.flex_center
+  display: flex
+  justify-content: center
+  align-items: center
+
+.chart_heading
+  font-size: 1.8rem
+  text-align: center
+  font-weight: bold
+  line-height: 1.67
+  color: #9b9b9b
 </style>
--- interviews/buzzni/src/store/index.js
+++ interviews/buzzni/src/store/index.js
@@ -7,6 +7,11 @@ const store = new Vuex.Store({
   state: {
     surveyData: null,
     surveyActionData: null,
+    selected: {
+      gender: null,
+      age: [],
+      question: null
+    }
   },
   mutations: {
     SURVEYDATA (state, payload) {
@@ -14,6 +19,12 @@ const store = new Vuex.Store({
     },
     SURVEYACTIONDATA (state, payload) {
       state.surveyActionData = payload
+    },
+    SELECTGENDER (state, payload) {
+      state.selected.gender = payload
+    },
+    SELECTQUESTION (state, payload) {
+      state.selected.question = payload
     }
   },
   actions: {
@@ -22,6 +33,12 @@ const store = new Vuex.Store({
     },
     updateSurveyActionData ({commit}, payload) {
       commit('SURVEYACTIONDATA', payload);
+    },
+    selectGender ({commit}, payload) {
+      commit('SELECTGENDER', payload)
+    },
+    selectQuestion ({commit} ,payload) {
+      commit('SELECTQUESTION', payload)
     }
   },
   getters: {
